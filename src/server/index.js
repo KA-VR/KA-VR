671facef7b883fdaccbd0f90b5cc062dc2071465
Alex <alex.lee3851@gmail.com>
2016-06-20 15:32:32 -0700

    fix bundle.js

--- src/server/index.js
+++ src/server/index.js
@@ -3,12 +3,12 @@ export default `
   <html>
     <head>
       <title>KAVR</title>
-      <script src="bundle.js"></script>
     </head>
     <body>
       <h1>Hello, I am KAVR</h1>
 
       <div id="app"></div>
+      <script src="bundle.js"></script>
     </body>
   </html>
 `;
