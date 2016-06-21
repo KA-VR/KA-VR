7248812879af9bb2caf6acd10789f03455d41304
Kent <kentqlee@gmail.com>
2016-06-21 16:53:48 -0700

    Built out basic routing between signin, signup, and dashboard pages

--- src/client/utils/router.js
+++ src/client/utils/router.js
@@ -7,18 +7,22 @@ import {
 } from 'react-router';
 
 // Layouts
-import AppContainer from '../containers/AppContainer';
+import MainLayout from '../layouts/MainLayout';
 import PortalLayout from '../layouts/PortalLayout';
 
 // Pages
 import SignInContainer from '../containers/SignInContainer';
-// import SignUpContainer from '../containers/SignUpContainer';
-// import DashboardContainer from '../containers/DashboardContainer';
+import SignUpContainer from '../containers/SignUpContainer';
+import DashboardContainer from '../containers/DashboardContainer';
 
 export default (
   <Router history={browserHistory}>
-    <Route component={AppContainer}>
-      <Route path="/signin" component={PortalLayout}><SignInContainer /></Route>
+    <Route component={MainLayout}>
+      <Route component={PortalLayout}>
+        <Route path="/" component={SignInContainer} />
+        <Route path="signup" component={SignUpContainer} />
+        <Route path="dashboard" component={DashboardContainer} />
+      </Route>
     </Route>
   </Router>
 );
