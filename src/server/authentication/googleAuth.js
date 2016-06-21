a02fd7956ce1f786ba0555190815ffd7460af18b
Victoria Tran <ptran.vicki@gmail.com>
2016-06-20 20:00:19 -0700

    Implement redis database instead of neo4j. Refactor neo4j queries into redis queries

--- src/server/authentication/googleAuth.js
+++ src/server/authentication/googleAuth.js
@@ -1,10 +1,12 @@
 import passport from 'passport';
 import GoogleStrategy from 'passport-google-oauth2';
 import keys from '../../../config.js';
-import apoc from 'apoc';
+import redis from 'redis';
 
 const clientID = keys.GOOGLE_CLIENT_ID;
 const clientSecret = keys.GOOGLE_CLIENT_SECRET;
+
+const db = redis.createClient();
 
 passport.use(new GoogleStrategy.Strategy({
   clientID,
@@ -17,23 +19,28 @@ passport.use(new GoogleStrategy.Strategy({
     // const lastName = profile.name.familyName;
     const fullName = 'Victoria Tran';
 
-    apoc.query('MATCH (n:User) where n.name = "%fullName%" return n', { fullName }).exec().then(
-      (response) => {
-        console.log('WORKING :D', response[0].data[0].row[0]);
-        // apoc.query('CREATE (n:User)').exec().then(
-        //   (response) => {
-        //     console.log('successfully added new user !!! response is:', response);
-        //   },
-        //   (fail2) => {
-        //     console.log('did not add a new user. error is:', fail2);
-        //   }
-        // );
-        done();
-      },
-      (fail) => {
-        console.log('cannot find user', fail);
+    db.get('name', (error, result) => {
+      if (error) {
+        // there is no name key
+        console.log('error: cannot find name key', error);
+      } else {
+        // name is found so auth is successful
+        if (result === fullName) {
+          console.log(result);
+          done();
+        } else {
+          // name is not found but key exists
+          // create new user
+          db.set('name', fullName, (error2, result2) => {
+            if (error2) {
+              console.log('error2: cannot set new user to name key', error);
+            } else {
+              console.log('successfully added new user! result2 is:', result2);
+            }
+          });
+        }
       }
-    );
+    });
   }
 ));
 
