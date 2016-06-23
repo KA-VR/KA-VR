a2040e407b9e40573b6236800568cf24e04f9664
Kent <kentqlee@gmail.com>
2016-06-23 14:35:41 -0700

    Add widget files

---
+++ src/client/components/TaskTextInput.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const TaskTextInput = () => (
+  <div>
+    <h5> TaskTextInput Field Here </h5>
+  </div>
+);
+
+export default TaskTextInput;
---
+++ src/client/components/TaskWidget.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import TaskTextInputContainer from '../containers/TaskTextInputContainer';
+import TaskInputContainer from '../containers/TaskInputContainer';
+
+const TaskWidget = () => (
+  <div>
+    <h5> TaskWidget </h5>
+    <TaskTextInputContainer />
+    <TaskInputContainer />
+  </div>
+);
+
+export default TaskWidget;
