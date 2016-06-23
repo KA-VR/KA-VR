2dab72789bcbb90be40ed8587d8e69a9cd1c8aee
Kent <kentqlee@gmail.com>
2016-06-23 14:35:04 -0700

    Render new widget to file

--- src/client/components/Widget.js
+++ src/client/components/Widget.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import TaskWidgetContainer from '../containers/TaskWidgetContainer';
 
 const Widget = (props) => (
   <div className="widget">
     <h5>Widgets Go Here</h5>
+    <TaskWidgetContainer />
     <button onClick={props.change}>Add Widget</button>
   </div>
 );
