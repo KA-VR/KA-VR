fc42e91305438a737b8ade03af4453a28c508e1e
Alex <alex.lee3851@gmail.com>
2016-06-22 23:54:47 -0700

    add materialize

--- src/server/index.js
+++ src/server/index.js
@@ -4,13 +4,12 @@ export default `
     <head>
       <title>KAVR</title>
       <script src="https://code.jquery.com/jquery-3.0.0.min.js" integrity="sha256-JmvOoLtYsmqlsWxa7mDSLMwa6dZ9rrIdtrrVYRnDRH0=" crossorigin="anonymous"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/js/materialize.min.js"></script>
-      <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/css/materialize.min.css" />
+      <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/css/materialize.min.css" /> 
     </head>
     <body>
       <div id="app"></div>
       <script src="bundle.js"></script>
-
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/js/materialize.min.js"></script>
     </body>
   </html>
 `;
