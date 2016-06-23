7e1293b803dc0f5f4f21d73ec8fd5ea555450d9a
Kent <kentqlee@gmail.com>
2016-06-22 19:29:12 -0700

    Create function to handle change

--- src/client/containers/SettingsContainer.js
+++ src/client/containers/SettingsContainer.js
@@ -6,13 +6,18 @@ class SettingsContainer extends Component {
   constructor(props) {
     super(props);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.saveSettings = this.saveSettings.bind(this);
   }
   handleSubmit() {
     browserHistory.push('/settings');
   }
+  saveSettings() {
+    // Pass props back to server
+    console.log('Settings Saved!');
+  }
   render() {
     return (
-      <Settings />
+      <Settings click={this.saveSettings}/>
     );
   }
 }
