45dd323a04ac9afe5ae28760319ab41512eaa92b
Kent <kentqlee@gmail.com>
2016-06-22 19:28:34 -0700

    Create function to allow change to user profile

--- src/client/containers/DashboardContainer.js
+++ src/client/containers/DashboardContainer.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
 import Dashboard from '../components/Dashboard';
+import { browserHistory } from 'react-router';
 
 class DashboardContainer extends Component {
   constructor(props) {
     super(props);
+    this.handleUserSettings = this.handleUserSettings.bind(this);
     this.handleUserProfile = this.handleUserProfile.bind(this);
   }
   handleUserProfile() {
     // Take in functions here
-    
+
+  }
+  handleUserSettings() {
+    browserHistory.push('/settings');
   }
   render() {
     return (
-      <Dashboard data={this.handleUserProfile} />
+      <Dashboard
+        data={this.handleUserProfile}
+        click={this.handleUserSettings}
+      />
     );
   }
 }
