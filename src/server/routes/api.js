93b2a2c15faa402263e61ef6018e16b921cdd797
Victoria Tran <ptran.vicki@gmail.com>
2016-06-23 19:11:18 -0700

    Add /api/token route to generate new access tokens

--- src/server/routes/api.js
+++ src/server/routes/api.js
@@ -1,13 +1,41 @@
+/* eslint-disable no-console*/
 import { Router } from 'express';
 import userController from '../controllers/userController';
 import authController from '../controllers/authController';
+import refresh from 'passport-oauth2-refresh';
+import strategy from '../authentication/googleStrategy';
+import Users from '../../../mysql.config';
+
 const router = new Router();
+
+refresh.use(strategy);
+
+const isAuthenticated = (req, res, next) => {
+  if (req.isAuthenticated()) return next();
+
+  return res.redirect('/');
+};
 
 /* Users */
 router.route('/api/user').get(userController.getAll);
 /* Authentication */
 
 router.route('/auth/google').get(authController.googleAuth);
+
+router.route('/api/token').get(isAuthenticated, (req, res) => {
+  Users.findOne({
+    where: {
+      email: req.user.email,
+    },
+  })
+  .then(result => result.refreshToken)
+  .then(token => {
+    refresh.requestNewAccessToken('google', token, (err, accessToken) => {
+      req.user.accessToken = accessToken;
+      res.send({ accessToken });
+    });
+  });
+});
 
 router.route('/auth/google/callback').get(authController.googleRedirect);
 
