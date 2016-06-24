896fa045bdda9f656f036a3e6564164dbf89e216
Kent <kentqlee@gmail.com>
2016-06-24 15:47:18 -0700

    Add components for task widget

--- src/client/components/TaskTextInput.js
+++ src/client/components/TaskTextInput.js
@@ -1,9 +1,64 @@
-import React from 'react';
+import React, { Component, PropTypes } from 'react';
+import classnames from 'classnames';
 
-const TaskTextInput = () => (
-  <div>
-    <h5> TaskTextInput Field Here </h5>
-  </div>
-);
+class TaskTextInput extends Component {
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      text: this.props.text || '',
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleBlur = this.handleChange.bind(this);
+  }
+
+  handleSubmit(e) {
+    // Enter key event listener
+    const text = e.target.value.trim();
+    if (e.which === 13) {
+      this.props.onSave(text);
+      if (this.props.newTask) {
+        this.setState({ text: '' });
+      }
+    }
+  }
+
+  handleChange(e) {
+    this.setState({ text: e.target.value });
+  }
+
+  handleBlur(e) {
+    if (!this.props.newTask) {
+      this.props.onSave(e.target.value);
+    }
+  }
+
+  render() {
+    return (
+      <input
+        className={
+          classnames({
+            edit: this.props.editing,
+            'new-task': this.props.newTask,
+          })}
+        type="text"
+        placeholder={this.props.placeholder}
+        autoFocus="true"
+        value={this.state.text}
+        onBlur={this.handleBlur}
+        onChange={this.handleChange}
+        onKeyDown={this.handleSubmit}
+      />
+    );
+  }
+}
+
+TaskTextInput.propTypes = {
+  onSave: PropTypes.func.isRequired,
+  text: PropTypes.string,
+  placeholder: PropTypes.string,
+  editing: PropTypes.bool,
+  newTask: PropTypes.bool,
+};
 
 export default TaskTextInput;
