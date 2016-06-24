7de02c1669e1068dee181865cc8a4d464084c7f5
Kent <kentqlee@gmail.com>
2016-06-24 15:47:50 -0700

    Add containers for task widget files

--- src/client/containers/TaskTextInputContainer.js
+++ src/client/containers/TaskTextInputContainer.js
@@ -1,15 +1,33 @@
-import React, { Component } from 'react';
+import React, { PropTypes, Component } from 'react';
 import TaskTextInput from '../components/TaskTextInput';
 
 class TaskTextInputContainer extends Component {
   constructor(props) {
     super(props);
+    this.handleSave = this.handleSave.bind(this);
   }
+  handleSave(text) {
+    if (text.length !== 0) {
+      this.props.addTask(text);
+    }
+  }
+
   render() {
     return (
-      <TaskTextInput />
+      <header className="header">
+        <h1>TASKS</h1>
+        <TaskTextInput
+          newTask
+          onSave={this.handleSave}
+          placeholder="What needs to be done?"
+        />
+      </header>
     );
   }
 }
+
+TaskTextInputContainer.propTypes = {
+  addTask: PropTypes.func.isRequired,
+};
 
 export default TaskTextInputContainer;
--- src/client/containers/TaskWidgetContainer.js
+++ src/client/containers/TaskWidgetContainer.js
@@ -1,16 +1,44 @@
-import React, { Component } from 'react';
-import TaskWidget from '../components/TaskWidget';
+import React, { Component, PropTypes } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import TaskTextInputContainer from '../containers/TaskTextInputContainer';
+import CurrentTaskList from '../components/CurrentTaskList';
+import * as TaskActions from '../actions';
 
 class TaskWidgetContainer extends Component {
   constructor(props) {
     super(props);
+    this.state = {};
   }
-
   render() {
+    const { tasks, actions } = this.props;
     return (
-      <TaskWidget />
+      <div>
+        <TaskTextInputContainer addTask={actions.addTask} />
+        <CurrentTaskList tasks={tasks} actions={actions} />
+      </div>
     );
   }
 }
 
-export default TaskWidgetContainer;
+TaskWidgetContainer.propTypes = {
+  tasks: PropTypes.array.isRequired,
+  actions: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => (
+  {
+    tasks: state.tasks,
+  }
+);
+
+const mapDispatchToProps = (dispatch) => (
+  {
+    actions: bindActionCreators(TaskActions, dispatch),
+  }
+);
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(TaskWidgetContainer);
