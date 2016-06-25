77308fc666c1244910b63d38d4104e82d537e0e9
Kent <kentqlee@gmail.com>
2016-06-25 16:13:05 -0700

    Add dump.rdb file

---
+++ src/client/services/auth.js
@@ -0,0 +1,56 @@
+/* eslint-disable no-console*/
+import $ from 'jquery';
+
+const signin = (userInfo, callback) => {
+  $.ajax({
+    url: '/signin',
+    type: 'POST',
+    dataType: 'json',
+    data: userInfo,
+  })
+  .done(res => {
+    console.log('RES:', res);
+    // Apply additional actions to our response from server
+    return res;
+  })
+  .then(data => {
+    console.log('DATA in SIGNIN: ', data);
+    // Invoke our callback
+    if (callback) {
+      callback(data);
+    }
+  })
+  .catch(err => {
+    console.error('Error on signup', err);
+  });
+};
+
+const signup = (userInfo, callback) => {
+  $.ajax({
+    url: '/signup',
+    type: 'POST',
+    dataType: 'json',
+    data: userInfo,
+  })
+  .done(res => {
+    return res;
+  })
+  .then(data => {
+    if (callback) {
+      callback(data);
+    }
+  })
+  .catch(err => {
+    console.error('Error on signup', err);
+  });
+};
+
+const logout = () => {
+
+};
+
+export default {
+  signin,
+  signup,
+  logout,
+};
