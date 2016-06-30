be3f1131a68eee495e9058d9da95a0a15e7b7f3d
Alex <alex.lee3851@gmail.com>
2016-06-30 11:45:39 -0700

    init

--- src/client/constants/ActionTypes.js
+++
@@ -1,6 +0,0 @@
-// Task Widget Actions
-
-export const ADD_TASK = 'ADD_TASK';
-export const REMOVE_TASK = 'REMOVE_TASK';
-export const EDIT_TASK = 'EDIT_TASK';
-export const COMPLETE_TASK = 'COMPLETE_TASK';
--- src/client/index.js
+++ src/client/index.js
@@ -1,15 +1,4 @@
-// import React from 'react';
 import ReactDOM from 'react-dom';
-import Router from './utils/router';
-// import Index from '../server/index.js';
-// import Speech from './components/SpeechToTextContainer.js';
+import App from './components/App';
 
-// Stylesheets
-import './stylesheets/style.scss';
-import './stylesheets/userstats.scss';
-import './stylesheets/widget.scss';
-import './stylesheets/chatbox.scss';
-import './stylesheets/avatar.scss';
-import './stylesheets/signin.scss';
-
-ReactDOM.render(Router, document.getElementById('app'));
+ReactDOM.render(App, document.getElementById('app'));
