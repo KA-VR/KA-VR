73b6b9f0d22e10a20aa979d206c0867a601a319b
Kent <kentqlee@gmail.com>
2016-07-02 15:20:31 -0700

    Add dashboard to App

--- src/client/app/App.js
+++ src/client/app/App.js
@@ -2,9 +2,13 @@ import React from 'react';
 import SpeechContainer from '../speech/SpeechContainer';
 import CanvasContainer from '../canvas/CanvasContainer';
 import ModalContainer from '../modals/ModalContainer';
+import LeftDashboardContainer from '../dashboard/LeftDashboardContainer';
+import RightDashboardContainer from '../dashboard/RightDashboardContainer';
 
 const App = () => (
   <div>
+    <LeftDashboardContainer />
+    <RightDashboardContainer />
     <ModalContainer />
     <CanvasContainer />
     <SpeechContainer />
