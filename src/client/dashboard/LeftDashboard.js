a9a14050fd64cfd797ed8f178dcd0cb386afe40b
Kent <kentqlee@gmail.com>
2016-07-03 15:24:26 -0700

    Create view for left side of dashboard

--- src/client/dashboard/LeftDashboard.js
+++ src/client/dashboard/LeftDashboard.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 // Import calendar, settings components
 const LeftDashboard = (props) => (
-  <div class="leftdashboard">
+  <div className="leftdashboard">
     <div>Calendar</div>
     <div>Settings</div>
   </div>
