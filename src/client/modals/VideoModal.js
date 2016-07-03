c60b46a1d524ea07d43ce0e60dbb552730059f65
rodaan <rodaan.rabang@gmail.com>
2016-07-02 17:21:17 -0700

    Updated video modal

--- src/client/modals/VideoModal.js
+++ src/client/modals/VideoModal.js
@@ -1,17 +1,30 @@
 import React, { PropTypes } from 'react';
 
+const resize = () => {
+  const height = window.innerHeight / 2;
+  return height;
+}
+
+const stopVideo = () => {
+  $('.video').leanModal({
+     complete: function() {
+      var $if = $(e.delegateTarget).find('iframe');
+      var src = $if.attr("src");
+      $if.attr("src", '/empty.html');
+      $if.attr("src", src);
+     } // Callback for Modal close
+   }
+ );
+}
+
 const VideoModal = (props) => (
-    <div id="video" className="modal bottom-sheet">
+    <div id="video" className="modal">
       <div className="modal-content">
-        <h4>Video Modal</h4>
-        <p>Standard</p>
-          <iframe width="560" height="315" src={props.modalState.video} frameborder="0" allowfullscreen></iframe>
-        <div className="col s3">
-        </div>
+          <iframe width="100%" height={resize()} src={props.modalState.video} frameborder="0" allowfullscreen></iframe>
       </div>
       <div className="modal-footer">
         <a href="#!" className=" modal-action modal-close waves-effect waves-green btn-flat">
-          Agree
+          Close
         </a>
       </div>
     </div>
