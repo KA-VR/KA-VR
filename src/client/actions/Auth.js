f76ac541e847154b6981a9f55a89c563fd4a552c
Kent <kentqlee@gmail.com>
2016-07-03 15:21:57 -0700

    Create Auth file

---
+++ src/client/actions/Auth.js
@@ -0,0 +1,25 @@
+import {
+  USER_SIGNIN,
+  USER_SIGNUP,
+  USER_SIGNOUT,
+} from '../actions/ActionTypes';
+
+// Create auth action creators
+const userSignin = (type, data, state) => {
+
+};
+
+const userSignup = (type, data, state) => {
+
+};
+
+const userSignout = (type, data, state) => {
+
+};
+
+// Create auth action functions
+  // Signin
+
+  // Signup
+
+  // Logout
