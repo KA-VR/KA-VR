988e6236ec77f7310694b280aa6112b6449bc196
Kent <kentqlee@gmail.com>
2016-07-03 21:37:46 -0700

    Define initial reducer functions to be created

--- src/client/reducers/Auth.js
+++ src/client/reducers/Auth.js
@@ -1,7 +1,46 @@
-// Create reducer functions
+import {
+  USER_SIGNIN,
+  USER_SIGNUP,
+  USER_SIGNOUT,
+} from '../actions/ActionTypes';
 
-  // Signup
+const authState = (state = {
+  state: 'none',
+  signedIn: null,
+}, action) => {
+  switch (action.type) {
+    case USER_SIGNIN:
+      return Object.assign({}, state, {
+        state: action.state,
+        data: action.data,
+      });
+    case USER_SIGNUP:
+      return Object.assign({}, state, {
+        state: action.state,
+        data: action.data,
+      });
+    case USER_SIGNOUT:
+      return Object.assign({}, state, {
+        state: action.state,
+        data: action.data,
+      });
+    default:
+      return state;
+  }
+};
 
-  // Signin
+const leftDashboardState = (state = {
+  state: 'none',
+  signedIn: false,
+}, action) => {
 
-  // Logout
+};
+
+const rightDashboardState = (state = {
+  state: 'none',
+  signedIn: false,
+}, action) => {
+
+};
+
+export { authState, leftDashboardState, rightDashboardState };
