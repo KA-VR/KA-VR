016fcbeef09f949579635df32653e921500c9653
Kent <kentqlee@gmail.com>
2016-07-03 21:36:15 -0700

    Import proptypes from react

--- src/client/dashboard/RightDashboard.js
+++ src/client/dashboard/RightDashboard.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 
 // Import calendar, settings components
-const RightDashboard = (props) => (
+const RightDashboard = () => (
   <div className="rightdashboard col s12 m6 l3">
     <div>Task Widget</div>
     <div>Placeholder Widget</div>
   </div>
 );
+
+RightDashboard.propTypes = {
+
+};
 
 export default RightDashboard;
