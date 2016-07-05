785bd3501db5b1550e23fa877fd18caf6b5586b3
Kent <kentqlee@gmail.com>
2016-07-04 21:29:39 -0700

    Add conditions for different rendering logic

--- src/client/app/App.js
+++ src/client/app/App.js
@@ -1,16 +1,32 @@
-import React from 'react';
-import SpeechContainer from '../speech/SpeechContainer';
+import React, { PropTypes } from 'react';
+// import SpeechContainer from '../speech/SpeechContainer';
 import CanvasContainer from '../canvas/CanvasContainer';
 import ModalContainer from '../modals/ModalContainer';
 import DashboardLayout from '../dashboard/DashboardLayout';
+import SignInContainer from '../auth/SignInContainer';
+import SignUpContainer from '../auth/SignUpContainer';
+// import SignUpContainer from '../auth/SignUpContainer';
 
-const App = () => (
-  <div>
-    <CanvasContainer />
-    <DashboardLayout />
-    <SpeechContainer />
-    <ModalContainer />
-  </div>
-);
+const App = ({ authState }) => {
+  console.log(authState);
+  // If the sign up view is true, render
+  if (authState.currentPage === 'signUp') {
+    return <SignUpContainer />;
+  }
+  if (authState.signedIn) {
+    return (
+      <div>
+        <CanvasContainer />
+        <DashboardLayout />
+        <ModalContainer />
+      </div>
+    );
+  }
+  return <SignInContainer />;
+};
+
+App.propTypes = {
+  authState: PropTypes.object,
+};
 
 export default App;
