5bcfeb9e6a29bb8a754f8df8527550e63706a2a8
Victoria Tran <ptran.vicki@gmail.com>
2016-07-05 12:24:33 -0700

    Include quotes component

--- src/client/app/App.js
+++ src/client/app/App.js
@@ -1,11 +1,11 @@
+/* eslint-disable no-console */
 import React, { PropTypes } from 'react';
-// import SpeechContainer from '../speech/SpeechContainer';
 import CanvasContainer from '../canvas/CanvasContainer';
 import ModalContainer from '../modals/ModalContainer';
 import DashboardLayout from '../dashboard/DashboardLayout';
 import SignInContainer from '../auth/SignInContainer';
 import SignUpContainer from '../auth/SignUpContainer';
-// import SignUpContainer from '../auth/SignUpContainer';
+import QuotesContainer from '../quotes/QuotesContainer';
 
 const App = ({ authState }) => {
   console.log('Auth State: ', authState);
@@ -16,6 +16,7 @@ const App = ({ authState }) => {
   if (authState.currentPage === 'dashboard' || localStorage.getItem('KAVR')) {
     return (
       <div>
+        <QuotesContainer />
         <CanvasContainer />
         <DashboardLayout />
         <ModalContainer />
