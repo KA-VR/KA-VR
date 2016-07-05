725d4ef6a028bbabf3c0b86f92d24f6cc1614cef
Kent <kentqlee@gmail.com>
2016-07-04 23:02:53 -0700

    Complete

--- src/client/auth/SignUpContainer.js
+++ src/client/auth/SignUpContainer.js
@@ -1,54 +1,26 @@
 /* eslint-disable no-console*/
-import React, { Component } from 'react';
-import serialize from 'form-serialize';
+import { connect } from 'react-redux';
 import SignUp from './SignUp';
-import auth from './auth';
+import { submitSignup, redirectSignIn } from '../actions/Auth';
 
-class SignUpContainer extends Component {
-  constructor(props) {
-    super(props);
-    this.handleSubmit = this.handleSubmit.bind(this);
+const mapStateToProps = (state) => {
+  const { authState } = state;
+  return {
+    authState,
+  };
+};
+
+const mapDispatchToProps = dispatch => ({
+  dispatch,
+  signUp: e => {
+    e.preventDefault();
+    return dispatch(submitSignup());
+  },
+  redirSignIn: e => {
+    e.preventDefault();
+    console.log('clicked');
+    return dispatch(redirectSignIn());
   }
+});
 
-  // componentWillMount() {
-  //   if (window.localStorage.getItem('KAVR')) {
-  //     browserHistory.push('/dashboard');
-  //   }
-  // }
-
-  handleSubmit() {
-    const form = document.querySelector('#signup');
-    const formData = serialize(form, { hash: true });
-    // check if the email supplied is valid
-    if (formData.email === undefined) {
-      console.log('email invalid');
-    } else if (formData.password === undefined) {
-      // check if the passwords entered matches
-      console.log('Please input password');
-    } else if (formData.password !== formData.passwordConfirm) {
-      console.log('PASSWORDS DO NOT MATCH');
-    } else if (formData.firstname === undefined) {
-      console.log('Please fill out first name');
-    } else if (formData.lastname === undefined) {
-      console.log('Please fill out last name');
-    } else {
-      // Successful form validation, check if user already exists
-      auth.signup(formData, (res) => {
-        console.log('RESPONSE on SIGNUP FROM SERVER: ', res);
-        if (res.response) {
-          console.log('Error signing up');
-        } else if (res.redirect === '/dashboard') {
-          window.location = res.redirect;
-        }
-      });
-    }
-  }
-
-  render() {
-    return (
-      <SignUp handleSubmit={this.handleSubmit} />
-    );
-  }
-}
-
-export default SignUpContainer;
+export default connect(mapStateToProps, mapDispatchToProps)(SignUp);
