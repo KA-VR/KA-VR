59f1e2999b748f2829f21849c2d5b78445d03b34
Kent <kentqlee@gmail.com>
2016-07-04 21:30:29 -0700

    Functional signin

--- src/client/actions/Auth.js
+++ src/client/actions/Auth.js
@@ -3,12 +3,14 @@ import {
   USER_SIGNUP,
   USER_SIGNOUT,
 } from '../actions/ActionTypes';
+import serialize from 'form-serialize';
+import auth from '../auth/auth';
 
 // Create auth action creators
-const userSignin = (type, data, state) => ({
+const userSignin = (type, userData, signedIn) => ({
   type,
-  data,
-  state,
+  userData,
+  signedIn,
 });
 
 const userSignup = (type, data, state) => ({
@@ -25,17 +27,28 @@ const userSignout = (type, data, state) => ({
 
 const submitSignin = () =>
   dispatch => {
-
+    const form = document.querySelector('#signin-form');
+    const formData = serialize(form, { hash: true });
+    console.log(form, formData);
+    auth.signin(formData, (res) => {
+      console.log('RESPONSE on SIGNIN FROM SERVER: ', res);
+      if (res.response) {
+        console.log('Error signing up');
+      } else if (res.redirect === '/dashboard') {
+        // window.location = res.redirect;
+        dispatch(userSignin(USER_SIGNIN, formData, true));
+      }
+    });
   };
 
 const submitSignup = () =>
   dispatch => {
-
+    dispatch(userSignup());
   };
 
 const executeSignout = () =>
   dispatch => {
-
+    dispatch(userSignout());
   };
 
 export {
