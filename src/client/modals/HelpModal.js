8388be3d3dfc5d5b4f54b6df21108238883c10a4
rodaan <rodaan.rabang@gmail.com>
2016-07-06 16:08:25 -0700

    Starting styling of canvas

--- src/client/modals/HelpModal.js
+++ src/client/modals/HelpModal.js
@@ -4,16 +4,18 @@ const HelpModal = () => (
   <div id="help" className="modal">
     <div className="modal-content">
       <h3>Help</h3>
-      <p>webSearch: 'search - Lets you search the web - ex. "search for dogs"'</p>
-      <p>imageSearch: 'search image - Lets you search for images - ex. "search image of dogs"'</p>
-      <p>newsSearch: 'search news - Lets you search for news articles -
+      <p>Web Search: 'search - Lets you search the web - ex. "search for dogs"'</p>
+      <p>Image Search: 'search image - Lets you search for images - ex. "search image of dogs"'</p>
+      <p>News Search: 'search news - Lets you search for news articles -
       ex. "search news of dogs"'</p>
-      <p>webOpen: 'open - Lets you go directly to a web site/application - ex. "open Facebook"'</p>
-      <p>playVideo: 'play video - Lets you play videos on the web - ex. "play video of dogs"'</p>
-      <p>calculate: 'calculate - Lets you make math calculations - ex. "calculate 5 + 5"'</p>
-      <p>weatherSearch: `search weather - Lets you find weather of a city -
+      <p>Web Open: 'open - Lets you go directly to a web site/application - ex. "open Facebook"'</p>
+      <p>Play Video: 'play video - Lets you play videos on the web - ex. "play video of dogs"'</p>
+      <p>Calculate: 'calculate - Lets you make math calculations - ex. "calculate 5 + 5"'</p>
+      <p>Weather Search: `search weather - Lets you find weather of a city -
       ex. "search weather of San Francisco"`</p>
-      <p>learning: `If you say something that KAVR is unsure about,
+      <p>Run Last Action: `run last action - Lets you run the last action you just did -
+      ex. "run last action"`</p>
+      <p>Learning: `If you say something that KAVR is unsure about,
       a survey will show up that will allow it to learn from you!`</p>
     </div>
     <div className="modal-footer">
