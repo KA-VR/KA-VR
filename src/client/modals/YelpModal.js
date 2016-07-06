ca67c5c552e459c885aa399769f7433fa4db5424
Kent <kentqlee@gmail.com>
2016-07-06 16:02:14 -0700

    Yelp modal now displays full search results

--- src/client/modals/YelpModal.js
+++ src/client/modals/YelpModal.js
@@ -1,25 +1,29 @@
 import React, { PropTypes } from 'react';
 
-const YelpModal = (props) => (
-  <div id="yelpsearch" className="modal">
-    <div className="modal-content">
-      <h4>Search</h4>
-      <ul className="collection">
-        {props.modalState.search.map(result => (
-          <li className="collection-item">
-            <a href={result.link} target="_blank">{result.link}</a>
-            <p>{result.description}</p>
-          </li>
-        ))}
-      </ul>
+const YelpModal = (props) => {
+  const shops = props.modalState.yelpsearchresults.shops;
+  console.log('SHOPS:', shops);
+  return (
+    <div id="yelp" className="modal">
+      <div className="modal-content">
+        <h4>Yelp Search Results:</h4>
+        <ul className="collection">
+          {shops.map((result, key) => (
+            <li key={key} className="collection-item">
+              <a href={result.url}><h5>{result.name}</h5></a>
+              <img src={result.image} alt="image"/>
+              <h6>{result.location}</h6>
+              <h6>{result.city}, {result.state} {result.zip}</h6>
+              <h6>Reviews: {result.reviews}</h6>
+              <h6>Rating: {result.rating}</h6>
+              <h6>Preview: {result.text}</h6>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
-    <div className="modal-footer">
-      <a href="#!" className=" modal-action modal-close waves-effect waves-green btn-flat">
-        Close
-      </a>
-    </div>
-  </div>
-);
+  );
+};
 
 YelpModal.propTypes = {
   modalState: PropTypes.object.isRequired,
