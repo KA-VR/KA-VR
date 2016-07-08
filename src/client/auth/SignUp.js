258cf12c3383e61c7fa24ecd67a067467736bb4c
Kent <kentqlee@gmail.com>
2016-07-08 12:35:48 -0700

    Styling changes

--- src/client/auth/SignUp.js
+++ src/client/auth/SignUp.js
@@ -2,36 +2,43 @@ import React, { PropTypes } from 'react';
 import Validation from 'react-validation';
 
 const SignUp = ({ signUp, redirSignIn }) => (
-  <div>
-    <Validation.Form id="signup">
-      <input
-        type="text"
-        name="firstname"
-        placeholder="first name"
-      />
-      <input
-        type="text"
-        name="lastname"
-        placeholder="last name"
-      />
-      <input
-        type="password"
-        name="password"
-        placeholder="password"
-      />
-      <input
-        type="password"
-        name="passwordConfirm"
-        placeholder="confirm password"
-      />
-      <Validation.Input
-        name="email"
-        placeholder="email"
-        validations={[{ rule: 'isEmail' }]}
-      />
-    </Validation.Form>
-    <button className="submit-button waves-effect waves-light btn" onClick={signUp}>Submit</button>
-    <a onClick={redirSignIn}>already have an account ? Sign in</a>
+  <div className="signup">
+    <h1 className="center-align">Sign Up</h1>
+    <div className="container">
+      <div className="row">
+        <Validation.Form id="signup" className="col offset-m3 m6 s12">
+          <input
+            type="text"
+            name="firstname"
+            placeholder="first name"
+          />
+          <input
+            type="text"
+            name="lastname"
+            placeholder="last name"
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="password"
+          />
+          <input
+            type="password"
+            name="passwordConfirm"
+            placeholder="confirm password"
+          />
+          <Validation.Input
+            name="email"
+            placeholder="email"
+            validations={[{ rule: 'isEmail' }]}
+          />
+          <button className="submit-button waves-effect waves-light btn" onClick={signUp}>Submit</button>
+        </Validation.Form>
+        <div className="col s12 offset-m3 m6 link">
+          <a onClick={redirSignIn}>already have an account ? Sign in</a>
+        </div>
+      </div>
+    </div>
   </div>
 );
 
